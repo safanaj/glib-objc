241eb7a8ddeb35a25cebc5db3533c2845bab6e83
Brian J. Tarricone <brian@tarricone.org>
2009-07-06 15:30:15 -0700

    great renaming: GLIB prefix to GOC prefix.  doesn't compile

--- gobject-objc/GOCBoxedValue.h
+++ gobject-objc/GOCBoxedValue.h
@@ -1,32 +1,31 @@
 /*
  *  glib-objc - objective-c bindings for glib/gobject
  *
- *  Copyright (c) 2008 Brian Tarricone <bjt23@cornell.edu>
+ *  Copyright (c) 2008-2009 Brian Tarricone <brian@tarricone.org>
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; version 2 of the License ONLY.
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; version 2 of the License ONLY.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef __GLIB_OBJC_BOXED_VALUE_H__
-#define __GLIB_OBJC_BOXED_VALUE_H__
+#ifndef __GOC_BOXED_VALUE_H__
+#define __GOC_BOXED_VALUE_H__
 
-#if !defined(GLIB_OBJC_COMPILATION) && !defined(__IN_GLIB_OBJC_H)
-#error "Do not include GLIBBoxedValue.h directly, as this file may change or disappear in the future.  Include <glib-objc/glib-objc.h> instead."
+#if !defined(GLIB_OBJC_COMPILATION) && !defined(__IN_GOBJECT_OBJC_H)
+#error "Do not include GOCBoxedValue.h directly, as this file may change or disappear in the future.  Include <gobject-objc.h> instead."
 #endif
 
-#import <Foundation/Foundation.h>
 
-@interface GLIBBoxedValue : NSValue
+@interface GOCBoxedValue : NSValue
 {
 @private
     gpointer _boxedValue;
--- gobject-objc/gobject-objc-private.h
+++ gobject-objc/gobject-objc-private.h
@@ -1,27 +1,26 @@
 /*
  *  glib-objc - objective-c bindings for glib/gobject
  *
- *  Copyright (c) 2008 Brian Tarricone <bjt23@cornell.edu>
+ *  Copyright (c) 2008-2009 Brian Tarricone <brian@tarricone.org>
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; version 2 of the License ONLY.
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; version 2 of the License ONLY.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef __GLIB_OBJC_PRIVATE_H__
-#define __GLIB_OBJC_PRIVATE_H__
+#ifndef __GOBJECT_OBJC_PRIVATE_H__
+#define __GOBJECT_OBJC_PRIVATE_H__
 
 #include <glib-object.h>
-#import <Foundation/Foundation.h>
 
 G_BEGIN_DECLS
 
@@ -67,12 +66,7 @@ GType _glib_objc_gtype_from_signature(const char *objc_signature);
 BOOL _glib_objc_signatures_match(GType target_gtype,
                                  const char *objc_signature);
 #endif
-id <NSObject> _glib_objc_nsobject_from_gvalue(const GValue *value);
-BOOL _glib_objc_gvalue_from_nsobject(GValue *gvalue,
-                                     const id <NSObject> nsobject,
-                                     BOOL gvalue_needs_init);
-
 
 G_END_DECLS
 
-#endif  /* __GLIB_OBJC_PRIVATE_H__ */
+#endif  /* __GOBJECT_OBJC_PRIVATE_H__ */
